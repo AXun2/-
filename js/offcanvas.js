33d6016d5bfe99a37bf1d278ad67b864df37b8aa
AXun2 <66954361+AXun2@users.noreply.github.com>
2020-07-11 17:05:23 +0800

    Add files via upload

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -13,3 +13,23 @@ $(document).ready(function () {
   $('#myModal').on('hidden', function () {
     // do something…
   })
+
+  // 当网页向下滑动 20px 出现"返回顶部" 按钮
+  window.onscroll = function() {
+    scrollFunction()
+  };
+  
+  function scrollFunction() {
+      // console.log(100);
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 100) {
+          document.getElementById("btnTop").style.display = "block";
+      } else {
+          document.getElementById("btnTop").style.display = "none";
+      }
+  }
+  
+  // 点击按钮，返回顶部
+  function returnTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  }
